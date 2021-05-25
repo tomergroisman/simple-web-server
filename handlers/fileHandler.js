38566c630f4a32259dfb7006a2f2e555591581dd
Tomer Groisman <tomergroisman@gmail.com>
2021-05-25 14:27:19 +0300

    Add write local JSON file logic

--- handlers/fileHandler.js
+++ handlers/fileHandler.js
@@ -1,10 +1,15 @@
+var fs = require('fs');
+
 const fileHandler = {
   readJsonFromFile: () => {
     const data = require("../wines.json")
     return data
   },
   writeObjectToJson: (obj, filename) => {
-    // TODO
+    const json = JSON.stringify(obj, null, 2);
+    fs.writeFile(filename, json, (err) => {
+      if (err) return console.error(err);
+    });
   }
 }
 
